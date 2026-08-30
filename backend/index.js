b17bef1adf0b963a073b42fc41c3c4360d4907e9
Abdullah Khan <khan.abdullah.baksh@gmail.com>
2026-08-30 20:39:28 +0530

    fixed CORS issue

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ let dummyData = [
 ];
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     let body = '';
     req.on('data', (chunk) => {
         body += chunk.toString();
